ddeb4cfaf3023a6952c9a99c02c5f0f34c01b7ec
seantsukim <seantsukim@gmail.com>
2018-07-15 14:27:33 -0700

    playerMovement

---
+++ 2DTopDownShooter/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+	public float playerX;
+	public float playerY;
+	public float moveSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		playerX = transform.position.x;
+		playerY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Vertical") < 0.0f)
+		{
+			playerY = Input.GetAxis("Vertical") * moveSpeed;
+		}
+		else if (Input.GetAxis("Vertical") > 0.0f)
+		{
+			playerY = Input.GetAxis("Vertical") * moveSpeed;
+		}
+		else if (Input.GetAxis("Horizontal") < 0.0f)
+		{
+			playerX = Input.GetAxis("Horizontal") * moveSpeed;
+		}
+		else if (Input.GetAxis("Horizontal") > 0.0f)
+		{
+			playerX = Input.GetAxis("Horizontal") * moveSpeed;
+		}
+
+		transform.position = new Vector3(playerX, playerY, transform.position.z);
+	}
+}

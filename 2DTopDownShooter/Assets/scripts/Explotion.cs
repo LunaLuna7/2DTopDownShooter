b462a7bc169e421a7efb4cbda1990c6739f6e5a2
Luis <lfluna@uci.edu>
2018-07-15 15:52:14 -0700

    Bomb

---
+++ 2DTopDownShooter/Assets/scripts/Explotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explotion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit");
+        }
+    }
+}

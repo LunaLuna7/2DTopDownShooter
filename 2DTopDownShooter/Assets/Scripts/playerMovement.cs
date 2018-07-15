3ec37c989db5e4bece52ac031983381e637b0554
seantsukim <seantsukim@gmail.com>
2018-07-15 14:47:29 -0700

    improved movement
    player movement uses Input.GetAxisRaw

--- 2DTopDownShooter/Assets/Scripts/playerMovement.cs
+++ 2DTopDownShooter/Assets/Scripts/playerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
 	public float playerX;
 	public float playerY;
-	public float moveSpeed = 2.0f;
+	public float moveSpeed = 1.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,15 +15,15 @@ public class playerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis("Vertical") > 0.5f || Input.GetAxis("Vertical") < -0.5f)
+		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
 		{
-			playerY = Input.GetAxis("Vertical") * moveSpeed;
+			playerY = Input.GetAxisRaw("Vertical") * moveSpeed;
 			transform.Translate(new Vector3(0.0f, playerY, 0.0f));
 		}
 
-		if(Input.GetAxis("Horizontal") > 0.5f || Input.GetAxis("Horizontal") < -0.5f)
+		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
 		{
-			playerX = Input.GetAxis("Horizontal") * moveSpeed;
+			playerX = Input.GetAxisRaw("Horizontal") * moveSpeed;
 			transform.Translate(new Vector3(playerX, 0.0f, 0.0f));
 		}
 	}

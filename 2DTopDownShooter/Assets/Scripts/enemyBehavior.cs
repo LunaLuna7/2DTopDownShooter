0c5f0c0093aa55b64e2336bdd68c5fa00e615fae
seantsukim <seantsukim@gmail.com>
2018-07-15 17:44:32 -0700

    some documentation

--- 2DTopDownShooter/Assets/Scripts/enemyBehavior.cs
+++ 2DTopDownShooter/Assets/Scripts/enemyBehavior.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemyBehavior : MonoBehaviour {
+	[Header("Enemy Health")]
 	public int startingHealth = 1; //the max health of enemy gameObject
-	[HideInInspector]public int health; //current health of the enemy gameObject
+	public int currentHealth; //current health of the enemy gameObject
 
 	// Use this for initialization
 	void Start () {
 		//initalize the current health to starting health
-		health = startingHealth; 
+		currentHealth = startingHealth; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (health <= 0)
+		//if current health is 0, this object should die
+		if (currentHealth <= 0)
 		{
 			Die();
 		}
@@ -22,6 +24,7 @@ public class enemyBehavior : MonoBehaviour {
 
 	void Die()
 	{
+		//kill this object
 		Destroy(gameObject);
 	}
 }

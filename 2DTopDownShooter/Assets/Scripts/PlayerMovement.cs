096852f8e8275b1e9d26692e624f50f6b6211f80
syy1125 <syy1125@hotmail.com>
2019-01-14 13:16:09 -0800

    Clean up player movement scripts
    Clean up `PlayerMovement` to make relative movement optional.
    Clean up `PlayerRotation` so that player rotation doesn't feel so weird.

---
+++ 2DTopDownShooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using Boo.Lang;
+using UnityEngine;
+using UnityEngineInternal.Input;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 15.0f;
+	public bool smooth;
+
+	/// <summary>
+	/// If movement is set to `relative`, player movement would be based on the character's rotation instead of absolute directions. 
+	/// </summary>
+	public bool relative;
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 input = smooth
+			? new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))
+			: new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		if (input.magnitude > 1) input.Normalize();
+
+		if (relative) input = transform.TransformVector(new Vector2(input.y, -input.x));
+
+		transform.position += input * speed * Time.deltaTime;
+	}
+}
---
+++ 2DTopDownShooter/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+	public float speed = 5;
+	public bool instant;
+
+	void Update()
+	{
+		Vector2 mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		float mouseAngle = Mathf.Atan2(mouseDirection.y, mouseDirection.x) * Mathf.Rad2Deg;
+		Quaternion desiredRotation = Quaternion.AngleAxis(mouseAngle, Vector3.forward);
+
+		if (instant)
+		{
+			transform.rotation = desiredRotation;
+		}
+		else
+		{
+			transform.rotation = Quaternion.RotateTowards(
+				transform.rotation,
+				desiredRotation,
+				speed * Time.deltaTime * Mathf.Rad2Deg
+			);
+		}
+	}
+}
+
+[CustomEditor(typeof(PlayerRotation))]
+public class PlayerRotationEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		var script = target as PlayerRotation;
+		Debug.Assert(script != null);
+
+		if (script.instant) GUI.enabled = false;
+		script.speed = EditorGUILayout.FloatField("Speed", script.speed);
+		GUI.enabled = true;
+
+		script.instant = EditorGUILayout.Toggle("Instant", script.instant);
+	}
+}

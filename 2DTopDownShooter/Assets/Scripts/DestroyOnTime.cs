046add856140ed7c6aec3718f2f30b0a9c73c26f
syy1125 <syy1125@hotmail.com>
2019-01-13 22:09:15 -0800

    Clean up `DestroyOnTime`
    Clean up unused imports.
    Remove unnecessary `Update` function.

--- 2DTopDownShooter/Assets/Scripts/DestroyOnTime.cs
+++ 2DTopDownShooter/Assets/Scripts/DestroyOnTime.cs
@@ -1,17 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class DestroyOnTime : MonoBehaviour {
+public class DestroyOnTime : MonoBehaviour
+{
+	public float time;
 
-    public float time;
-	// Use this for initialization
-	void Start () {
-        Destroy(gameObject, time);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start()
+	{
+		Destroy(gameObject, time);
 	}
 }

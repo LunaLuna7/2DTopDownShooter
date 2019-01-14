4b45b493e52366bb22be13da8048e92ebbb810ab
syy1125 <syy1125@hotmail.com>
2019-01-13 22:36:07 -0800

    Document `DestroyOnTime`

--- 2DTopDownShooter/Assets/Scripts/DestroyOnTime.cs
+++ 2DTopDownShooter/Assets/Scripts/DestroyOnTime.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Automatically destroys the game object after a certain interval.
+/// Note that the timer only starts after the script is enabled.
+/// The destruction is not cancellable.
+/// </summary>
 public class DestroyOnTime : MonoBehaviour
 {
 	public float time;

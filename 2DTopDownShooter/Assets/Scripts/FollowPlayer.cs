160ce9ce3f0d64a57672950c091dc55e9c20d0be
syy1125 <syy1125@hotmail.com>
2019-01-16 13:24:46 -0800

    Camera follows player
    Implement script `FollowPlayer` that makes camera keep a consistent offset from the player.

---
+++ 2DTopDownShooter/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public GameObject player;
+
+	private Vector3 m_Offset;
+
+	private void Start()
+	{
+		m_Offset = transform.position - player.transform.position;
+	}
+
+	private void LateUpdate()
+	{
+		transform.position = player.transform.position + m_Offset;
+	}
+}

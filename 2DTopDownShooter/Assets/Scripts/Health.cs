349d63300bf03d63b0705323ef1d681e6de2c47c
syy1125 <syy1125@hotmail.com>
2019-01-16 13:57:06 -0800

    Refactor health handling
    `Health` script no longer exposes `health` for write. Instead, now it exposes two methods `Damage` and `Heal`.

--- 2DTopDownShooter/Assets/Scripts/Health.cs
+++ 2DTopDownShooter/Assets/Scripts/Health.cs
@@ -7,32 +7,38 @@ using UnityEngine.Events;
 public class Health : MonoBehaviour
 {
 	public int maxHealth;
-	private int m_Health;
-
-	public UnityEvent afterHealthChange;
+	public int health { get; private set; }
 	
-	public int health
-	{
-		get { return m_Health; }
-		set
-		{
-			m_Health = Mathf.Clamp(value, 0, maxHealth);
+	public UnityEvent afterHealthChange;
+	public UnityEvent onDeath;
 
-			afterHealthChange.Invoke();
-
-			if (m_Health <= 0)
-			{
-				Die();
-			}
-		}
-	}
 
 	private void Start()
 	{
 		health = maxHealth;
 	}
 
-	private void Die()
+	public void Damage(int amount)
+	{
+		health = Mathf.Clamp(health - amount, 0, maxHealth);
+
+		afterHealthChange.Invoke();
+
+		if (health <= 0)
+		{
+			onDeath.Invoke();
+		}
+	}
+
+	public void Heal(int amount)
+	{
+		health = Mathf.Clamp(health + amount, 0, maxHealth);
+		
+		afterHealthChange.Invoke();
+	}
+
+	// A common action is to destroy the object on death. Add this listener to the `onDeath` event to achieve that effect.
+	public void DestroyOnDeath()
 	{
 		Destroy(gameObject);
 	}

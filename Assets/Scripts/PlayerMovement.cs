44c02d0bc2177b3957615d1dc6200e93e1af6201
Luis Luna <lfluna@uci.edu>
2019-01-22 13:34:33 -0800

    changed scripts to be more beginner friendly

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float speed = 15.0f;
 	public bool smooth;
+    private Vector3 input;
 
 	/// <summary>
 	/// If movement is set to `relative`, player movement would be based on the character's rotation instead of absolute directions.
@@ -15,12 +16,27 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	private void FixedUpdate()
 	{
-		Vector3 input = smooth
-			? new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))
-			: new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-		if (input.magnitude > 1) input.Normalize();
 
-		if (relative) input = transform.TransformVector(new Vector2(input.y, -input.x));
+        if (smooth)
+        {
+            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+        else
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+
+        //One line version of the above code: Vector3 input = smooth ? new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) : new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (input.magnitude > 1)
+        {
+            input.Normalize();
+        }
+
+		if (relative)
+        {
+            input = transform.TransformVector(new Vector2(input.y, -input.x));
+        }
 
 		transform.position += input * speed * Time.deltaTime;
 	}
